57754e4596ee6ac20507ae01bfc45398b49a9a10
deependujha <deependujha21@gmail.com>
2023-05-24 00:57:09 +0530

    done & working

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,46 @@ import random
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import time
+from langchain.llms import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.chains import RetrievalQA
+import pinecone
 
 load_dotenv()
 PORT = os.getenv('PORT')
 
 app = Flask(__name__)
 CORS(app)
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_ENV = os.getenv('PINECONE_ENV')
+
+embeddings = OpenAIEmbeddings()
+
+# initialize pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=PINECONE_ENV
+)
+
+index_name = "kritrim-ai"
+llm = OpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+qa = RetrievalQA.from_chain_type(
+    llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever()
+)
+
+new_prompt = '''Use the following pieces of context to answer the question at the end. If the answer is not present in the context, please answer with "I am sorry, but I couldn't find the answer".
+
+{context}
+
+Question: {question}
+Helpful Answer:'''
+
+qa.combine_documents_chain.llm_chain.prompt.template = new_prompt
 
 
 @app.route('/')
@@ -25,24 +58,17 @@ def home():
         response.status_code = 400
         return response
 
-    messages = [
-        "Hello, how are you?",
-        "What's your favorite color?",
-        "Have a great day!",
-        "I'm feeling tired today.",
-        "Do you enjoy reading?",
-        "What's your favorite hobby?",
-        "Let's meet for coffee sometime.",
-        "I can't wait for the weekend!",
-        "Did you watch the latest movie?",
-        "Wishing you a wonderful week ahead!"
-    ]
+    try:
+        result = qa.run(question)
 
-    idx = random.randint(0, 9)
-    response = jsonify({'response': messages[idx], 'success': True})
-    response.status_code = 200
-    time.sleep(3)
-    return response
+        response = jsonify({'response': result, 'success': True})
+        response.status_code = 200
+        return response
+    
+    except Exception as e:
+        response = jsonify({'error': str(e), 'success': False})
+        response.status_code = 500
+        return response
 
 
 if __name__ == '__main__':
