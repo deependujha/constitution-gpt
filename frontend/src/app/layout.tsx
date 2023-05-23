6fb1d0ff5db5f0c5e364f9e1d4b510646362cef7
deependujha <deependujha21@gmail.com>
2023-05-24 01:44:35 +0530

    fixed typo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Roboto } from 'next/font/google';
 const inter = Roboto({ subsets: ['latin'], weight: '400' });
 
 export const metadata = {
-	title: 'Constituion  GPT',
+	title: 'Constitution  GPT',
 	description: 'A GPT-3.5 powered chatbot for the Indian Constitution',
 };
 
